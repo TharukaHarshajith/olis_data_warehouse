0a04f4d95cbbf271c546851f94fef96eafbbc523
Tharuka Harshajith <137327327+TharukaHarshajith@users.noreply.github.com>
2026-05-22 23:46:50 +0530

    Add fact_order_items view to gold schema
    Create or alter the fact_order_items view to include order, product, and seller details.

--- scripts/gold/ddl_gold_v2.sql
+++ scripts/gold/ddl_gold_v2.sql
@@ -14,3 +14,21 @@ SELECT
     order_delivered_customer_date,
     order_estimated_delivery_date
 FROM silver.olist_orders_dataset
+
+
+    
+CREATE OR ALTER VIEW gold.fact_order_items AS
+SELECT
+    d.order_key,
+    p.product_key,
+    s.seller_key,
+    oi.price,
+    oi.freight_value AS shipping_cost
+FROM silver.olist_order_items_dataset oi
+LEFT JOIN gold.dim_orders d
+ON oi.order_id = d.order_id
+LEFT JOIN gold.dim_products p
+ON oi.product_id = p.product_id
+LEFT JOIN gold.dim_sellers s
+ON oi.seller_id = s.seller_id
+
